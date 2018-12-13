ee7cbbe72f006302b2431ef734283c5227fce381
moshe770 <mshperling770@gmail.com>
2018-12-13 11:11:02 +0200

    release of 3.5.5

---
+++ app/code/local/Autocompleteplus/Autosuggest/Model/Rule.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Rule.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category Mage
+ *
+ * @package   Instantsearchplus
+ * @author    Fast Simon <info@instantsearchplus.com>
+ * @copyright 2018 Fast Simon (http://www.instantsearchplus.com)
+ * @license   Open Software License (OSL 3.0)*
+ * @link      http://opensource.org/licenses/osl-3.0.php
+ */
+
+/**
+ * Class Autocompleteplus_Autosuggest_Model_Rule
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category Mage
+ *
+ * @package   Instantsearchplus
+ * @author    Fast Simon <info@instantsearchplus.com>
+ * @copyright Fast Simon (http://www.instantsearchplus.com)
+ * @license   Open Software License (OSL 3.0)*
+ * @link      http://opensource.org/licenses/osl-3.0.php
+ */
+class Autocompleteplus_Autosuggest_Model_Rule extends Mage_CatalogRule_Model_Rule
+{
+    /**
+     * Callback function for product matching
+     *
+     * @param $args
+     * @return void
+     */
+    public function callbackValidateProduct($args)
+    {
+        $product = clone $args['product'];
+        $product->setData($args['row']);
+
+        $results = array();
+        foreach ($this->_getWebsitesMap() as $websiteId => $defaultStoreId) {
+            $product->setStoreId($defaultStoreId);
+            $valid = (int)$this->getConditions()->validate($product);
+            if ($valid) {
+                $results[$websiteId] = $valid;
+            }
+        }
+        if (count($results) > 0) {
+            $this->_productIds[$product->getId()] = $results;
+        }
+    }
+}
